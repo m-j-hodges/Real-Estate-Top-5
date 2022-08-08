25d8af6667d07e99fcf90d6526b7f5eea3826609
Matthew Hodges <matt.j.hodges@gmail.com>
2022-08-08 13:29:17 -0700

    working on routes to get new user creation working.

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+
+router.use('/',userRoutes)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
 // app.use(routes)
 
 
