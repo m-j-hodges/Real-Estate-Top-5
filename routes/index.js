cf3aeaed2676681b9204bcf5b750ca267d3ef5a9
Matthew Hodges <matt.j.hodges@gmail.com>
2022-08-08 17:21:09 -0700

    added some routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const apiRoutes = require('./apiRoutes')
 
 router.use('/',userRoutes)
-
+router.use('/api', apiRoutes)
 module.exports = router;
