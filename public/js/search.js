2b01e2ef6d5badb82f2787651d2abdb25d4554d6
Lucia Gil <luciagil5952@gmail.com>
2022-08-14 12:10:24 -0400

    Changed homepage

--- public/js/search.js
+++ public/js/search.js
@@ -1,20 +1,29 @@
 const searchBtn = document.getElementById("search-addon");
 const inputEl = document.getElementById("searchProperty");
-const url = "https://api.mashvisor.com/v1.1/client/city/properties/georgia/";
+const url = "https://api.mashvisor.com/v1.1/client/city/properties/ga/";
+
 
 // Not working yet
 searchBtn.addEventListener("click", function () {
    let input = inputEl.value;
+
+   
+
+
+
     fetch(`${url}${input}`, {
         headers: {
             "Access-Control-Allow-Origin": "https://api.mashvisor.com",
-            "x-api-key": "e9fc1589-f4cd-49a7-bfbb-b81e88d78837",
+            "x-api-key": "f206ffc1-0ffa-4f45-b13c-826b47e7b298",
         },
-        mode: "no-cors"
+        accept: "*/*",
+        mode: "no-cors",
+        credentials: 'include',
+        connection: "keep-alive"
     })
-    .then(data => data.json())
-    .then(result => {
-        console.log(result);
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
     })
    console.log(input);
 })
