1155d35c0cecef9516570cec4d3611ac8e5d500e
Matthew Hodges <matt.j.hodges@gmail.com>
2022-08-18 10:28:16 -0700

    missed 1 change

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 let loginButton = document.getElementById("login")
-const url = 'http://localhost:3001'
+const url = 'https://real-estate-top-5.herokuapp.com'
 let submitUsername
 let submitEmail
 let submitPassword
