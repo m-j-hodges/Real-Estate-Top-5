e71ad50d15010ed1ecae587d266e891fa2d91cf2
Michael Andrews <michael.andrews@pega.com>
2022-08-20 14:02:12 -0400

    deleted controller folder

--- controllers/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const userRoutes = require('./userRoutes')
-const projectRoutes = require('./projectRoutes')
-
-router.use('/', apiRouting)
-
-modules.export = router
